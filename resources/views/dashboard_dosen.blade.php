92f5eafc800086b24991343a9a1496460f642077
SukemaruMomo666 <prabuxmomo@gmial.com>
2025-10-29 15:36:18 +0700

    newbetdah

--- resources/views/dashboard_dosen.blade.php
+++ resources/views/dashboard_dosen.blade.php
@@ -1,11 +1,27 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <h1>dosen galak part 2</h1>
-</body>
-</html>
+    @extends('layouts.admin')
+
+    {{-- Mengatur judul halaman --}}
+    @section('title', 'Tentang Aplikasi')
+
+    {{-- Mulai bagian konten --}}
+    @section('content')
+    <div class="container-fluid px-4">
+        <h1 class="mt-4">Data Dosen</h1>
+        <ol class="breadcrumb mb-4">
+            <li class="breadcrumb-item"><a href="{{ route('dashboard') }}">Dashboard</a></li>
+            <li class="breadcrumb-item active">Dosen</li>
+        </ol>
+
+
+    @endsection
+    {{-- Akhir bagian konten --}}
+
+    {{-- Tambahkan CSS/JS khusus jika perlu --}}
+    @push('styles')
+    {{-- <style> h1 { color: blue; } </style> --}}
+    @endpush
+
+    @push('scripts')
+    {{-- <script> console.log('Halaman Tentang dimuat!'); </script> --}}
+    @endpush
+    
--- resources/views/mahasiswa.blade.php
+++ resources/views/mahasiswa.blade.php
@@ -1,11 +1,27 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <h1>nama saya mahasiswa rajin</h1>
-</body>
-</html>
+    @extends('layouts.admin')
+
+    {{-- Mengatur judul halaman --}}
+    @section('title', 'Tentang Aplikasi')
+
+    {{-- Mulai bagian konten --}}
+    @section('content')
+    <div class="container-fluid px-4">
+        <h1 class="mt-4">Profile Mahasiswa</h1>
+        <ol class="breadcrumb mb-4">
+            <li class="breadcrumb-item"><a href="{{ route('dashboard') }}">Dashboard</a></li>
+            <li class="breadcrumb-item active">Mahasiswa</li>
+        </ol>
+
+
+    @endsection
+    {{-- Akhir bagian konten --}}
+
+    {{-- Tambahkan CSS/JS khusus jika perlu --}}
+    @push('styles')
+    {{-- <style> h1 { color: blue; } </style> --}}
+    @endpush
+
+    @push('scripts')
+    {{-- <script> console.log('Halaman Tentang dimuat!'); </script> --}}
+    @endpush
+    
