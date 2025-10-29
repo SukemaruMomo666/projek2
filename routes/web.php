91a1c4e9dd4f2d5071a1da9c81885fe6f61e38e0
SukemaruMomo666 <prabuxmomo@gmial.com>
2025-10-29 14:34:33 +0700

    view

--- routes/web.php
+++ routes/web.php
@@ -13,8 +13,8 @@ Route::get('/', function () {
     return view('auth.login');
 });
 
-Route::get('/home', function () {
-    return view('home');
+Route::get('/dashboard', function () {
+    return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
